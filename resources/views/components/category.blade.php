32142706f412078e38f95b7bbf63e7580322a457
Nathi Taengnim <>
2022-03-25 20:54:17 +0000

    create category model

---
+++ resources/views/components/category.blade.php
@@ -0,0 +1,11 @@
+<div class="ml-4">
+    
+    <a href="/categories/{{ $category->slug }}" class="{{ $category->depth === 0 ? 'font-bold': '' }}">{{ $category->title }}</a>
+  
+    @foreach ($category->children as $child) 
+        <div class="ml-4">
+            <x-category :category="$child" />
+        </div> 
+    @endforeach
+  
+</div>
