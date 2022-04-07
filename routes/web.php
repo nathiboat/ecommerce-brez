9952006677e46c352733419417bb253574616b5d
Nathi Taengnim <>
2022-04-07 23:21:13 +0100

    category show

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CartIndexController;
+use App\Http\Controllers\CategoryShowController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductShowController;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class);
 
 Route::get('/cart', CartIndexController::class);
+
+Route::get('/categories/{category:slug}', CategoryShowController::class);
 
 
 Route::get('/products/{product:slug}', ProductShowController::class);
