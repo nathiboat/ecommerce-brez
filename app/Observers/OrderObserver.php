84c4cbb2ba3dcde35524a4b6e5f39048790bb1d1
Nathi Taengnim <>
2023-01-23 10:39:55 +0000

    order status update

---
+++ app/Observers/OrderObserver.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Order;
+
+class OrderObserver
+{
+    public function updated(Order $order)
+    {
+        $originalOrder = new Order(
+            collect($order->getOriginal())
+                ->only($order->statuses)
+                ->toArray()
+        );
+
+        $filledStatuses = collect($order->getDirty())
+            ->only($order->statuses)
+            ->filter(fn ($status) => filled($status));
+
+        if($originalOrder->status() !== $order->status() &&  $filledStatuses->count()){
+
+        }
+    }
+}
